47cbd1a106c1710b64c57ffda69299a725adc3ab
serii <seriiburduja@gmail.com>
2023-12-09 16:14:26 +0200

    feat: git stagged

--- git.sh
+++ git.sh
@@ -2,8 +2,8 @@
 
 args=("$@")
 
+date_now=$(date +"%Y-%m-%d %H:%M:%S")
 function gitUpdateAll(){
-  date_now=$(date +"%Y-%m-%d %H:%M:%S")
   dirs=(~/Documents/Knowledge-base ~/xubuntu ~/.password-store ~/.config/nvim ~/i3wm-home ~/i3wm-office ~/Documents/bash-scripts ~/Documents/bash-apps ~/Documents/bash-private)
   for dir in "${dirs[@]}"; do
     echo "=========================================="
@@ -17,26 +17,45 @@ function gitUpdateAll(){
   done
 }
 
-select action in "nvim" "clipboard" "update"
+function gitStagged(){
+  for dir in $(find . -type d -name ".git"); do
+    cd "$dir/.."
+    if [ -n "$(git status --porcelain)" ]; then
+      echo "dir: $dir"
+      git reset --hard
+      git add .
+      git commit -m "updated by script at $date_now"
+      git pull
+      git push
+    fi  
+    cd - > /dev/null
+  done
+}
+
+select action in "nvim" "clipboard" "update" "stagged"
 do
-    case $action in
-        nvim)
-           $(git log --pretty="%C(Yellow)%h  %C(reset)%ad (%C(Green)%cr%C(reset))%x09 %C(Cyan)%an: %C(reset)%s" --date=short -30 --reverse > log.log)
-            bat log.log
-            break
-            ;;
-        clipboard)
-            log=$(git log --since="3am" --pretty=tformat:"%s" --reverse > log.log);
-            cat log.log | xclip -selection clipboard
-            rm log.log
-            break
-            ;;
-        update)
-          gitUpdateAll
-          break
-          ;;
-        *)
-            echo "Invalid option"
-            ;;
-    esac
+  case $action in
+    nvim)
+      $(git log --pretty="%C(Yellow)%h  %C(reset)%ad (%C(Green)%cr%C(reset))%x09 %C(Cyan)%an: %C(reset)%s" --date=short -30 --reverse > log.log)
+      bat log.log
+      break
+      ;;
+    clipboard)
+      log=$(git log --since="3am" --pretty=tformat:"%s" --reverse > log.log);
+      cat log.log | xclip -selection clipboard
+      rm log.log
+      break
+      ;;
+    update)
+      gitUpdateAll
+      break
+      ;;
+    stagged)
+      gitStagged
+      break
+      ;;
+    *)
+      echo "Invalid option"
+      ;;
+  esac
 done
