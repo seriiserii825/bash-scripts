aa81661692fcdde9e1740c5de16eb2616f0f5c7c
serii <seriiburduja@gmail.com>
2023-12-12 09:39:33 +0200

    feat: conver-fonts finished

--- convert-fonts.sh
+++ convert-fonts.sh
@@ -1,88 +1,90 @@
 #! /bin/bash -x
 
-echo "add ttf or woff and woff2 files in current directory"
+function ttfToWoff2() {
+  if ls *.ttf 1> /dev/null 2>&1; then
+    for file in *.ttf; do
+      woff2_compress "$file"
+      ttf2woff "$file" "${file%.*}.woff" 
+    done
+    rm *.ttf
+  else
+    echo "${tmagenta}no ttf files found${treset}"
+  fi
+}
 
-# check if has ttf or woff files in current directory
-if [[ ! $(find . -name "*.woff" || -name "*.woff2") ]]; then
-    echo "No TTF, WOFF, or WOFF2 files found in the directory."
-    exit 1
-fi
+function woffToCss(){
+  if ls *.woff 1> /dev/null 2>&1; then
+    touch fonts.css
+    for file in *.woff; do
+      # Extract font name from the file name by removing the extension and any style information
+      font_original="${file%.*}"
+      font_name=$(echo "$font_original" | tr '[:upper:]' '[:lower:]')
 
-touch fonts.css
+      font_style="normal"
+      font_weight="normal"
 
-##find zip files
-#if ls *.zip 1> /dev/null 2>&1; then
-#  # Loop through the zip files and unzip them
-#  for zip_file in *.zip; do
-#    # Unzip the file into the same directory
-#    unzip "$zip_file"
-#  done
-#  rm *.zip
-#else
-#  echo "No zip files found in the directory."
-#fi
+      if [[ $font_name == *"italic"* ]]; then
+        font_style="italic"
+      fi
 
-##find ttf files
+      if [[ $font_name == *"thin"* ]]; then
+        font_weight="100"
+      elif [[ $font_name == *"extralight"* ]]; then
+        font_weight="200"
+      elif [[ $font_name == *"light"* ]]; then
+        font_weight="300"
+      elif [[ $font_name == *"medium"* ]]; then
+        font_weight="500"
+      elif [[ $font_name == *"semibold"* || $font_name == *"demibold"* ]]; then
+        font_weight="600"
+      elif [[ $font_name == *"bold"* ]]; then
+        font_weight="700"
+      elif [[ $font_name == *"extrabold"* ]]; then
+        font_weight="800"
+      elif [[ $font_name == *"black"* || $font_name == *"heavy"* ]]; then
+        font_weight="900"
+      else
+        font_weight="400"
+      fi
+      # echo "font_weight: $font_weight"
+      font_name="${font_name%%-*}"  # Remove any style info
+      capital_name="${font_name^}"
 
-#if ls *.ttf 1> /dev/null 2>&1; then
-#  for file in *.ttf; do
-#    woff2_compress "$file"
-#    ttf2woff "$file" "${file%.*}.woff" 
-#  done
-#  # remove all .ttf files
-#  rm *.ttf
-#fi
+      echo "@font-face {
+      font-family: '$capital_name'; 
+      src: url('assets/fonts/${file%.*}.woff2') format('woff2'),
+      url('assets/fonts/${file%.*}.woff') format('woff');
+      font-weight: $font_weight;
+      font-style: $font_style;
+      font-display: swap;
+      }" >> fonts.css
+    done
+    bat fonts.css
+    cat fonts.css | xclip -selection clipboard
+    rm fonts.css
+  else
+    echo "${tmagenta}no woff files found${treset}"
+  fi
+}
 
-#for file in *.woff; do
-#  # Extract font name from the file name by removing the extension and any style information
-#  font_original="${file%.*}"
-#  font_name=$(echo "$font_original" | tr '[:upper:]' '[:lower:]')
+select action in info convert
+do
+  case $action in
+    info)
+      echo "${tgreen}Add to current directory ttf or woff and woff2 files${treset}"
+      echo "${tblue}This script will convert them to woff2 and woff and generate css file${treset}"
+      echo "${tyellow}css file will be copied to clipboard${treset}"
+      break
+      ;;
+    convert)
+      ttfToWoff2
+      woffToCss
+      break
+      ;;
+    *)
+      echo "${tmagenta}invalid option${treset}"
+      ;;
+  esac
+done
 
-#  # # Initialize font-style and font-weight variables
-#  font_style="normal"
-#  font_weight="normal"
 
-#  # # Check if the font name contains "Italic" and set font-style accordingly
-#  if [[ $font_name == *"italic"* ]]; then
-#    font_style="italic"
-#  fi
-
-#  # Check for specific font weights and set font-weight accordingly
-#  if [[ $font_name == *"thin"* ]]; then
-#    font_weight="100"
-#  elif [[ $font_name == *"extralight"* ]]; then
-#    font_weight="200"
-#  elif [[ $font_name == *"light"* ]]; then
-#    font_weight="300"
-#  elif [[ $font_name == *"medium"* ]]; then
-#    font_weight="500"
-#  elif [[ $font_name == *"semibold"* || $font_name == *"demibold"* ]]; then
-#    font_weight="600"
-#  elif [[ $font_name == *"bold"* ]]; then
-#    font_weight="700"
-#  elif [[ $font_name == *"extrabold"* ]]; then
-#    font_weight="800"
-#  elif [[ $font_name == *"black"* || $font_name == *"heavy"* ]]; then
-#    font_weight="900"
-#  else
-#    font_weight="400"
-#  fi
-#  # echo "font_weight: $font_weight"
-#  font_name="${font_name%%-*}"  # Remove any style info
-#  capital_name="${font_name^}"
-
-#  # # Generate CSS rule with extracted font name, font-style, and font-weight
-#  echo "@font-face {
-#  font-family: '$capital_name'; 
-#  src: url('assets/fonts/${file%.*}.woff2') format('woff2'),
-#  url('assets/fonts/${file%.*}.woff') format('woff');
-#  font-weight: $font_weight;
-#  font-style: $font_style;
-#  font-display: swap;
-#}" >> fonts.css
-#done
-
-#bat fonts.css
-
-#cat fonts.css | xclip -selection clipboard
-#rm fonts.css
