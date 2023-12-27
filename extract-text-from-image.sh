53f1ecd666ce8ad36940e4fa2838fc985ce36618
serii <seriiburduja@gmail.com>
2023-12-27 17:52:51 +0200

    feat: comment extract

--- extract-text-from-image.sh
+++ extract-text-from-image.sh
@@ -2,8 +2,8 @@
 
 tesseract ~/Downloads/screen.jpg ~/Downloads/screen
 sed '/^[[:space:]]*$/d' ~/Downloads/screen.txt | xclip -selection clipboard
-rm ~/Downloads/screen.jpg
-rm ~/Downloads/screen.txt
+# rm ~/Downloads/screen.jpg
+# rm ~/Downloads/screen.txt
 
 
 #bindsym $mod+ctrl+y exec --no-startup-id maim -f jpg -s ~/Downloads/screen.jpg && exec ~/Documents/bash-scripts/extract-text-from-image.sh
