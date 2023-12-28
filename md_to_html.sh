5e2e3f9aab02f318cb4c40e969f51c2ac70f7d49
serii <seriiburduja@gmail.com>
2023-12-28 12:35:28 +0200

    upd: md

--- md_to_html.sh
+++ md_to_html.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 cd ~/Downloads
 md_path=~/Downloads/index.md
-html_path=~/Downloads/index.html
 touch $md_path
-touch $html_path
 
 echo "$(xclip -o -selection clipboard)" > $md_path
-# bat $md_path
-pandoc -f markdown index.md > index.html
-# bat $html_path
-xclip -selection clipboard $html_path
+bat $md_path
+pandoc --columns=1000 -f markdown $md_path | xclip -selection clipboard
 
 
