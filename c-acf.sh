bf16eebcd8706ecdd6f0c0b56cc4671f58a795e4
serii <seriiburduja@gmail.com>
2023-12-28 18:39:51 +0200

    updated by script at 2023-12-28 18:39:28

--- c-acf.sh
+++ c-acf.sh
@@ -2,24 +2,40 @@
 
 file_path=~/Downloads/test/home.json
 
-function generateId(){
-  CHARACTER_SET="AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789"
-  password=""
-  for i in $(seq 1 13); do
-    random_byte=$(od -An -N1 -i /dev/urandom)
-    index=$((random_byte % ${#CHARACTER_SET}))
-    password="${password}${CHARACTER_SET:index:1}"
-  done
-  return $password | tr -d '\n' | xsel -b -i 
+function newGroup(){
+  id="$(openssl rand -base64 12)"
+  name=$1
+  slug=$(echo $name | tr '[:upper:]' '[:lower:]' | tr ' ' '_')
+  result=$(cat $file_path | jq '.[0].fields[.[0].fields| length] += {
+  "key": "field_'${id}'",
+  "label": "'${name}'",
+  "name": "'${slug}'",
+  "aria-label": "",
+  "type": "group",
+  "instructions": "",
+  "required": 0,
+  "conditional_logic": 0,
+  "wrapper": {
+  "width": "",
+  "class": "",
+  "id": ""
+},
+"placement": "top",
+"endpoint": 0,
+"date": "2010-01-07T19:55:99.999Z",
+"xml": "xml_samplesheet_2017_01_07_run_09.xml",
+"status": "OKKK",
+"message": "metadata loaded into iRODS successfullyyyyy"
+}')
+echo $result > $file_path
 }
 
 function newTab(){
+  id="$(openssl rand -base64 12)"
   name=$1
-  echo $name
   slug=$(echo $name | tr '[:upper:]' '[:lower:]' | tr ' ' '_')
-  echo $slug
   result=$(cat $file_path | jq '.[0].fields[.[0].fields| length] += {
-  "key": "field_333d8274aa9d6",
+  "key": "field_'${id}'",
   "label": "'${name}'",
   "name": "'${slug}'",
   "aria-label": "",
@@ -40,18 +56,8 @@ function newTab(){
 "message": "metadata loaded into iRODS successfullyyyyy"
 }')
 echo $result > $file_path
+newGroup $name
 }
 
-# file_path=~/Downloads/test/home.json
-# file_content=$(fzf)
-# file_content=$(cat ~/Downloads/home.json)
-# bat $file_content
-# jq '.[] | .fields' $file_path
-
-# result=$(cat $file_path | jq . += newTab 'Test')
-newTab 'Test'
-# echo $result
-# jq '.[]'
-
-# jq '.[0].fields[3]' $file_path
-
+read -p "Enter the name of the tab: " tab_name
+newTab $tab_name
